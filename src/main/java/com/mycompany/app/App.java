00facd7e7a923fbd71f475b01f43558535ec4b1c
EIANNIVG6 <vincenzo.iannini@nttdata.com>
2025-12-29 00:28:06 +0100

    third changement

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World  second changement!";
+    private final String message = "Hello World  third changement!";
     
     public App() {}
 
